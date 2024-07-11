0df3cf38bb33ba3703986b1440715375c3c34a0c
LarsArnth <laaj@cloudfactory.dk>
2024-07-11 10:28:31 +0200

    added title

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -1,4 +1,4 @@
-
+#Bootstrap.ps1
 # function to test if the script is running as admin
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
