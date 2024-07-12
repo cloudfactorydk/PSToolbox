2ac89d473f7afea04dba383a33c5a3c8b50aeddf
LarsArnth <laaj@cloudfactory.dk>
2024-07-12 11:24:45 +0200

    test

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -20,9 +20,9 @@ Invoke-RestMethod "https://raw.githubusercontent.com/cloudfactorydk/PSToolbox/ma
 
 $arguments="-file $scriptPath -executionpolicy bypass"
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Start-Process powershell -runAs -ArgumentList $arguments -NoExit
+    Start-Process powershell -runAs -NoExit -ArgumentList $arguments 
 }
 else {
-    Start-Process powershell -ArgumentList $arguments -NoExit
+    Start-Process powershell -NoExit -ArgumentList $arguments 
 }
 
